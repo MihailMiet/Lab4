f460b0bdc4ef1a396c219b0ef1c930da250de8f3
Mihail Mihail <studymiet0@outlook.com>
2025-05-30 17:48:58 +0300

    Lab4_v1

--- main.c
+++ main.c
@@ -1,41 +1,52 @@
-//Задать массив целых двузначных чисел. 
-// Создать массив из неповторяющихся чисел исходного массива, вывести его на экран, в результирующем массиве не должно быть повторов.
+//Дана целочисленная матрица 14x14.
+//Найти максимум в заштрихованной области.
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
 int main(void) {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int M[13][13], indexdif=1, lcount=0, hcount=0, max=0;
+	srand(time(NULL));
+	int M[14][14], indexdif = 0, lcount = 0, hcount = 0, max = 0, fillflag=0, num=1;
 	while (hcount < 14) {
 		while (lcount < 14) {
 			M[hcount][lcount] = rand();
 			lcount++;
 		}
+		lcount = 0;
 		hcount++;
 	}
+	lcount = 0, hcount = 0;
+	while (lcount < 153) { printf("-"); lcount++; }
+	printf("-\n");
 	lcount = 0, hcount = 0;
 	while (hcount < 14) {
 		while (lcount < 13) {
-			printf("%d ", M[hcount][lcount]);
+			printf("%9d |", M[hcount][lcount]);
 			lcount++;
+			num++;
 		}
-		printf("%d \n", M[hcount][lcount + 1]);
+		lcount = 0;
+		printf("%9d |\n", M[hcount][lcount + 1]);
+		while (lcount < 153) { printf("-"); lcount++; }
+		printf("-\n");
+		lcount = 0;
 		hcount++;
+		num++;
 
 	}
+	num = 0;
 	lcount = 0, hcount = 0;
 	while (hcount < 14) {
 		while (lcount + indexdif < 14) {
-			if (M[hcount][lcount + indexdif] > M[hcount][lcount + indexdif - 1]) { max = M[hcount][lcount + indexdif]; }
+			if (M[hcount][lcount + indexdif] > max) { max = M[hcount][lcount + indexdif]; if (lcount > hcount) { num = lcount * hcount; } }
 			lcount++;
-			indexdif++;
 		}
+		indexdif++;
+		lcount = 0;
 		hcount++;
 	}
-	lcount = 0, hcount = 0;
-	while (lcount < 16) { printf("-"); lcount++; }
-	printf("-\n");
 	printf("%d", max);
 	return 0;
 }
